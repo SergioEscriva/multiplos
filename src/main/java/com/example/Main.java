374bdc0ac1d7c8b8c0f3900d05da3a177e6fa71a
Sergio Escrivá <sergioescrivap@gmail.com>
2024-07-16 23:51:08 +0200

    Refactorizado: eliminado arraylist sin uso

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,8 +1,5 @@
 package com.example;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Main {
     public static void main(String[] args) {
         numeros();
@@ -12,17 +9,15 @@ public class Main {
         int start = 1;
         int end = 999;
         int sumNumbers = 0;
-        List<Integer> numberList = new ArrayList<>();
 
         for (int i = start; i <= end; i++) {
             if (i % 3 == 0 || i % 5 == 0) {
                 sumNumbers = sumNumbers + i;
-                numberList.add(i);
+
             }
 
         }
 
-        // System.out.println(numberList);
         System.out.println("Número Inicial: " + start + " Número Final: " + end);
         System.out.println("Suma total múltiplos 3 y 5: " + sumNumbers);
     }
