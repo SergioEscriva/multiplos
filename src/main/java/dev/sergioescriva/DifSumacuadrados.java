1f49b71174d80c1585913b5278436ca74675d159
Sergio Escrivá <sergioescrivap@gmail.com>
2024-07-22 16:27:42 +0200

    Refactorizado

--- src/main/java/dev/sergioescriva/DifSumacuadrados.java
+++ src/main/java/dev/sergioescriva/DifSumacuadrados.java
@@ -8,11 +8,19 @@ public class DifSumacuadrados {
         int sumCuadrado = 0;
 
         for (int numero = 0; numero <= end; numero++) {
+
+            // suma de los cuadrados
             sumNumbers += numero * numero;
+
+            // suma de los números
             sumCuadrado += numero;
 
         }
+
+        // cuadrado de la suma de los números
         int cuadrado = sumCuadrado * sumCuadrado;
+
+        // diferencia de los resultados de los cuadrados.
         int dif = cuadrado - sumNumbers;
         System.out.println("Suma Cuadrados: " + sumNumbers + " | Cuadrado de la Suma: " + sumCuadrado * sumCuadrado
                 + " | Diferecina Cuadrados:  " + dif);
